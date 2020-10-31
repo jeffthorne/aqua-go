599638ed1442264174268138363d86ad6ecb91f2
jeffthorne <jthorne@u.washington.edu>
2020-10-31 12:41:25 -0700

    first commit

--- README.go
+++ README.go
@@ -6,7 +6,8 @@ Status: Experimental<br/>
 
 Documentation
 ----
-https://godoc.org/github.com/jeffthorne/aqua-go<br/>
+https://godoc.org/github.com/jeffthorne/aqua-go
+\
 
 Installation
 ----
@@ -17,5 +18,7 @@ Install: go get -u github.com/jeffthorne/aqua-go
 Usage
 ----
 
-Default usage against a secure endpoint with InsecureSkipVerify set to true <br/><br/>
+Default usage against a secure endpoint with InsecureSkipVerify set to true 
+\
+\
 aqua, _ := aqua.NewClient("192.168.1.52", 443, "user id", "password")
