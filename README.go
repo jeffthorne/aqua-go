93cd2084b23126752d3adec734175fe28a622514
jeffthorne <jthorne@u.washington.edu>
2020-10-31 12:43:31 -0700

    first commit

--- README.go
+++ README.go
@@ -1,24 +1,20 @@
 # aqua-go
 
-A fantastic GO SDK for Aqua's Enterprise Platform<br/><br/>
-
-Status: Experimental<br/>
+A fantastic GO SDK for Aqua's Enterprise Platform
+Status: Experimental
 
 Documentation
 ----
 https://godoc.org/github.com/jeffthorne/aqua-go
-\
+
 
 Installation
 ----
-
 Install: go get -u github.com/jeffthorne/aqua-go
 
 
 Usage
 ----
-
 Default usage against a secure endpoint with InsecureSkipVerify set to true 
-\
-\
+
 aqua, _ := aqua.NewClient("192.168.1.52", 443, "user id", "password")
